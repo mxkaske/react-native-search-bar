b370f568921ccda1f0bdb21350ef16dd81a9fa2d
Maximilian Kaske <maximilian@kaske.org>
2021-01-27 00:29:59 +0100

    chore: update example

--- example/src/screens/Home.tsx
+++ example/src/screens/Home.tsx
@@ -1,28 +1,34 @@
 import * as React from 'react';
-import { StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
-import ReanimatedExpoBuilderBob, {
-  SearchBar,
-  StickyBar,
-} from 'react-native-search-bar';
+import {
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+  Text,
+} from 'react-native';
+import { SearchBar, StickyBar } from 'react-native-search-bar';
 import { Feather } from '@expo/vector-icons';
+import type { TextInput } from 'react-native';
+import faker from 'faker';
 
 const Home = () => {
+  const inputRef = React.useRef<TextInput>(null);
   return (
     <KeyboardAvoidingView
       behavior="padding"
       style={styles.KeyboardAvoidingContainer}
     >
       <SearchBar
+        ref={inputRef}
         searchIcon={<Feather name="search" color="black" size={20} />}
-        clearIcon={<Feather name="x" color="black" size={20} />}
+        clearIcon={<Feather name="x-circle" color="black" size={20} />}
         placeholder="Type here..."
         onClear={() => console.log('clear')}
         autoCorrect={false}
       />
       <ScrollView style={styles.container} keyboardDismissMode="on-drag">
-        <ReanimatedExpoBuilderBob />
+        <Text>{faker.lorem.paragraphs(8)}</Text>
       </ScrollView>
-      <StickyBar />
+      <StickyBar inputRef={inputRef} />
     </KeyboardAvoidingView>
   );
 };
@@ -30,10 +36,11 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
   },
   KeyboardAvoidingContainer: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: 'gold',
   },
 });
 
